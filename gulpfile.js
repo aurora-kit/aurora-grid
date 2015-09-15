877cce61cc21c1645fa7a63830a713acf02237b3
Lionel - MBP <elrumordelaluz@hotmail.com>
2015-09-15 18:02:58 +0200

    Fix duplicated gulp-size pipe

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,6 @@ gulp.task('scss', function() {
   return gulp.src('src/**/*.scss')
     .pipe(plumber({errorHandler: onError}))
     .pipe(sass())
-    .pipe(size({ gzip: true, showFiles: true }))
     .pipe(header(banner, { package : package }))
     .pipe(autoprefixer())
     .pipe(csscomb())
@@ -46,7 +45,6 @@ gulp.task('scss', function() {
     .pipe(rename({suffix: '.min'}))
     .pipe(cssmin())
     .pipe(size({ gzip: true, showFiles: true }))
-    .pipe(size({ gzip: false, showFiles: true }))
     .pipe(gulp.dest('dist/'))
     .pipe(notify({
       onLast: true,
